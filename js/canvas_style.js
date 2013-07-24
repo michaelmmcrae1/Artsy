45effd004b19ede4dd2275ad5adaa3a522d49f3f
Michael McRae <michaelmmcrae1@gmail.com>
2013-07-23 21:13:19 -0500

    After a night of coding -- fixed up hexCode -> RGB functionality. Began to allow for user to change canvas style.

---
+++ js/canvas_style.js
@@ -0,0 +1,32 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function($) {
+    
+    $('#canary').click(function() {
+        $('body').css('background-image', 'url(images/canvas-1.jpg)');
+    });
+    
+    $('#orange').click(function() {
+        $('body').css('background-image', 'url(images/canvas-3.png)');
+    });
+    
+    /*$('#canary').click(function() {
+        $('body').css('background-image', 'url(images/canvas-1.jpg)');
+    });
+    
+    $('#canary').click(function() {
+        $('body').css('background-image', 'url(images/canvas-1.jpg)');
+    });
+    
+    $('#canary').click(function() {
+        $('body').css('background-image', 'url(images/canvas-1.jpg)');
+    });
+    
+    $('#canary').click(function() {
+        $('body').css('background-image', 'url(images/canvas-1.jpg)');
+    });*/
+    
+});
